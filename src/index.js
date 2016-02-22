812e045c11281b64a44fca9dec561584c156bb2d
Tanix <pwdtnx@gmail.com>
2016-02-22 15:03:33 +0900

    add: convert indexers of string literals

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
+import _isNaN from 'lodash.isnan';
+
 export default function({ types: t, template }) {
+  const buildStringIndexer = template(`STRING.charAt(INDEX)`);
+
   return {
     visitor: {
       MemberExpression: {
         exit(path) {
+          const { node, parent, scope } = path;
+
+          const {
+            object: o,
+            property: p
+          } = node;
+
+          if(t.isStringLiteral(o)) {
+            if(t.isNumericLiteral(p) ||
+               t.isStringLiteral(p) && !_isNaN(+p.value)) {
+              // "str"[0], "str"["1"]
+              path.replaceWith(
+                buildStringIndexer({
+                  STRING: o,
+                  INDEX: t.NumericLiteral(+p.value)
+                })
+              );
+            }
+          }
         }
       }
     }
